9ee23b66d8c4f2d0a01694b4430f4cdd4c8276b8
hermann wenninger <hermann23@vivaldi.net>
2021-11-02 20:57:39 +0100

    #pagination and PostListView

--- django_projets/artblog/models.py
+++ django_projets/artblog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -15,7 +20,17 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    #manager
+    objects = models.Manager() # The default manager.
+    published = PublishedManager()## custom Manager
+
+
     class Meta:
         ordering = ('-publish',)
     def __str__(self):
         return self.title
+
+    
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.publish.year,self.publish.month,self.publish.day, self.slug])
