06cc18075593a35d52884545971f1bf4f9e147d3
hermann wenninger <hermann23@vivaldi.net>
2021-11-04 18:42:39 +0100

    #taggit in detail view and html

--- django_projets/artblog/views.py
+++ django_projets/artblog/views.py
@@ -98,8 +98,9 @@ def post_detail(request, year, month, day, post):
         similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
         similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
     return render(request,
-                  'artblog/post/detail.html',
-                  {'post': post,
-                   'comments': comments,
-                   'new_comment': new_comment,
-                   'comment_form': comment_form})
+              'artblog/post/detail.html',
+              {'post': post,
+               'comments': comments,
+               'new_comment': new_comment,
+               'comment_form': comment_form,
+               'similar_posts': similar_posts})
