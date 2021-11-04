3fc0ddc58f7e142015d64de2bcc547ec992e86dd
hermann wenninger <hermann23@vivaldi.net>
2021-11-04 18:34:46 +0100

    #counting tacks similar posts inviews

--- django_projets/artblog/urls.py
+++ django_projets/artblog/urls.py
@@ -3,7 +3,7 @@ from . import views
 app_name = 'artblog'
 urlpatterns = [
     # post views
-    path('/a', views.post_list, name='post_list'),
+    path('a/', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail,name='post_detail'),
